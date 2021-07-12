66ac5ce3655617ab0b0dc1a4b4411c1c738a9f2b
Chris Conner <chris.conner57@gmail.com>
2021-07-11 17:50:19 -0700

    Update Navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
-import { FileText } from 'phosphor-react';
 import { color, fontSize, Link } from '../Styles';
 
 const Nav = () => {
